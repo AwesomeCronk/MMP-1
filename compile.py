dbeddbb4a3e8a35faa084b4d05e7e312afc8f9be
Clayton Cronk <awesomecronk@gmail.com>
2023-04-04 22:15:17 -0500

    Got compiler working. Finished fibonacci.mmp.

--- compile.py
+++ compile.py
@@ -1,4 +1,4 @@
-import argparse, sys
+import argparse, os, sys
 
 
 def getArgs():
@@ -10,7 +10,7 @@ def getArgs():
     )
     parser.add_argument(
         '-o',
-        '--output',
+        '--outfile',
         type=str,
         default='output.bin',
         help='Output file'
@@ -22,10 +22,17 @@ def getArgs():
         default='MMP-1',
         help='Device to target'
     )
+    parser.add_argument(
+        '-l',
+        '--list',
+        action='store_true',
+        help='Generate program listing'
+    )
 
     args = parser.parse_args()
 
     return args
+
 
 def isValidTagName(name):
     validChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._'
@@ -33,6 +40,7 @@ def isValidTagName(name):
     for char in name:
         if not char in validChars: return False
     return True
+
 
 def resolveAddr(addr):
     if addr in tags:
@@ -59,88 +67,156 @@ def resolveValue(value):
 
 deviceLayouts = {
     'MMP-1': {
-        'ROM':      0x0000,
-        'RAM':      0x4000,
-        'ALU.A':    0xF000,
-        'ALU.B':    0xF001,
-        'ALU.SUM':  0xF002,
-        'ALU.AND':  0xF003,
-        'ALU.OR':   0xF004,
-        'ALU.XOR':  0xF005,
-        'ALU.NOT':  0xF006,
-        'ALU.GT':   0xF007,
-        'ALU.GTE':  0xF008,
-        'ALU.LT':   0xF009,
-        'ALU.LTE':  0xF00A,
-        'Step':     0xFFFF
+        'programOffset': 0x0000,
+        'maxProgramSize': 0x4000,
+        'tags': {
+            'ROM':      0x0000,
+            'RAM':      0x4000,
+            'ALU.A':    0xF000,
+            'ALU.B':    0xF001,
+            'ALU.SUM':  0xF002,
+            'ALU.AND':  0xF003,
+            'ALU.OR':   0xF004,
+            'ALU.XOR':  0xF005,
+            'ALU.NOT':  0xF006,
+            'ALU.GT':   0xF007,
+            'ALU.GTE':  0xF008,
+            'ALU.LT':   0xF009,
+            'ALU.LTE':  0xF00A,
+            'Step':     0xFFFF
+        }
     }
 }
 
 
 if __name__ == '__main__':
     args = getArgs()
-    try: tags = deviceLayouts[args.device]
+    try: tags = deviceLayouts[args.device]['tags']
     except IndexError: print('Device {} not recognized'); sys.exit(1)
-    valuesToWrite = {}
     binary = b''
-    program = []
+    program = {}
 
+
+    # Load code
     with open(args.infile, 'r') as infile:
-        for l, line in enumerate(infile.readlines()):
-            instr = line.split(';')[0].strip()  # Remove comments and clean up whitespace
-            if instr == '': continue
+        code = infile.read().split('\n')
+    print('Read {} ({} lines)'.format(args.infile, len(code)))
+
+
+    # First pass: Generate program object
+    for l, line in enumerate(code):
+        instr = line.split(';')[0].strip()  # Remove comments and clean up whitespace
+        if instr == '': continue
+
+
+        # Absolute tag
+        elif instr[0] == '$':
+            name, rawAddr = instr[1:].strip().split()
+            if name in tags.keys(): print('ERROR Line {}: Tag {} declared multiple times'.format(l + 1, name)); sys.exit(1)
+
+            addr = resolveAddr(rawAddr)
+            if addr is None: print('ERROR Line {}: Cannot resolve address {}'.format(l + 1, rawAddr))
+            tags[name] = addr
+
+            # print('Line {}: Declare absolute tag {} at 0x{:04X}'.format(l + 1, name, addr) + ('' if value is None else ' with value {}'.format(value)))
+
+
+        # Relative tag
+        elif instr[0] == '@':
+            if '=' in instr:
+                name, rawValue = [part.strip() for part in instr[1:].split('=')]
+            else:
+                name = instr[1:].strip(); rawValue = None
+
+            if name in tags.keys():
+                print('ERROR Line {}: Tag {} declared multiple times'.format(l + 1, name)); sys.exit(1)
+
+            addr = len(program)
+
+            if not rawValue is None:
+                value = resolveValue(rawValue)
+                if value is None: print('ERROR Line {}: Invalid value {}'.format(l + 1, rawValue))
+                program[l] = value
+
+            tags[name] = addr
+
+            # print('Line {}: Declare relative tag {} at 0x{:04X}'.format(l + 1, name, addr) + ('' if value is None else ' with value {}'.format(value)))
+
+
+        # Copy
+        else:
+            src, dest = [part.strip() for part in instr.split()]
+
+            program[l] = (src, dest)
+
+            # print('Line {}: Copy {} to {}'.format(l + 1, src, dest))
+
+
+    # Second pass: Replace tag names with their resolutions
+    for l in program.keys():
+        instr = program[l]
+        if isinstance(instr, tuple):
+            src, dest = [resolveAddr(part) for part in instr]
             
-            elif instr[0] == '$':
-                if '=' in instr:
-                    declaration, rawValue = [part.strip() for part in instr[1:].split('=')]
-                    value = resolveValue(rawValue)
-                    if value is None: print('ERROR Line {}: Absolute tag {} has invalid value {}'.format(l + 1, name, rawValue)); sys.exit(1)
+            if src is None: print('ERROR Line {}: Unable to resolve address for {}'.format(l + 1, instr[0])); sys.exit(1)
+            if dest is None: print('ERROR Line {}: Unable to resolve address for {}'.format(l + 1, instr[1])); sys.exit(1)
 
+            program[l] = (src, dest)
+
+    # Third pass: Generate binary
+    binary = b''
+    for instr in program.values():
+        if isinstance(instr, tuple):
+            binary = binary + int.to_bytes(instr[0], 2, 'big') + int.to_bytes(instr[1], 2, 'big')
+        elif isinstance(instr, int):
+            binary = binary + int.to_bytes(instr, 4, 'big')
+
+
+    # Write binary
+    with open(args.outfile, 'wb') as outfile:
+        outfile.write(binary)
+    
+    binarySize = len(binary)
+    prefixes = ['B', 'kB', 'MB', 'GB', 'TB', 'PB']
+
+    for prefix in prefixes:
+        if binarySize < 1024:
+            print('Wrote {} ({}{})'.format(args.outfile, binarySize, prefix))
+            break
+        else:
+            binarySize //= 1024
+
+
+    # Generate program listing
+    if args.list:
+        terminalWidth = os.get_terminal_size()[0]
+        compiledWidth = 16
+        lineNumSize = len(str(len(code)))   # Max number of digits in line numbers
+        print(' Addr  : {} | Line : Source'.format('Compiled'.center(compiledWidth)))
+        print('====== : {} | ==== : {}'.format('=' * compiledWidth, '=' * (terminalWidth - compiledWidth - 19)))
+        for l, line in enumerate(code):
+            if l in program.keys():
+                addr = tuple(program.keys()).index(l)
+
+                compiled = program[l]
+                if isinstance(compiled, tuple):
+                    src, dest = compiled
+                    if isinstance(src, int): src = '0x{:04X}'.format(src)
+                    if isinstance(dest, int): dest = '0x{:04X}'.format(dest)
+                    compiled = '{} -> {}'.format(str(src).rjust((compiledWidth - 4) // 2), str(dest).ljust((compiledWidth - 4) // 2))
+                
+                elif isinstance(compiled, int):
+                    compiled = '0x{:08X}'.format(compiled).ljust(compiledWidth)
+
+                # Should never get here if passes 1 and 2 worked, but hey it never hurts to add an else
                 else:
-                    declaration = instr[1:].strip(); value = None
-
-                name, rawAddr = declaration.split()
-                if name in tags.keys(): print('ERROR Line {}: Tag {} declared multiple times'.format(l + 1, name)); sys.exit(1)
-
-                addr = resolveAddr(rawAddr)
-                if addr is None: print('ERROR Line {}: Absolute tag {} has invalid address {}'.format(l + 1, name, rawAddr)); sys.exit(1)
-
-                if not value is None:
-                    valuesToWrite[addr] = value
-
-                tags[name] = addr
-
-                print('Line {}: Declare absolute tag {} at 0x{:04X}'.format(l + 1, name, addr) + ('' if value is None else ' with value {}'.format(value)))
-
-            elif instr[0] == '@':
-                if '=' in instr:
-                    name, value = [part.strip() for part in instr[1:].split('=')]
-                else:
-                    name = instr[1:].strip(); value = None
-
-                if name in tags.keys():
-                    print('ERROR Line {}: Tag {} declared multiple times'.format(l + 1, name)); sys.exit(1)
-
-                addr = len(program)
-
-                if not value is None:
-                    program.append(value)
-
-                tags[name] = addr
-
-                print('Line {}: Declare relative tag {} at 0x{:04X}'.format(l + 1, name, addr) + ('' if value is None else ' with value {}'.format(value)))
+                    compiled = repr(compiled).ljust(compiledWidth)
 
             else:
-                rawSrc, rawDest = [part.strip() for part in instr.split()]
+                compiled = ' ' * compiledWidth
+                addr = None
 
-                src = resolveAddr(rawSrc)
-                dest = resolveAddr(rawDest)
+            if addr is None: addr = ' ' * 8
+            else: addr = '0x{:04X} :'.format(addr)
 
-                if src is None: print('ERROR Line {}: Invalid source {}'.format(l + 1, rawSrc)); sys.exit(1)
-                if dest is None: print('ERROR Line {}: Invalid destination {}'.format(l + 1, rawDest)); sys.exit(1)
-
-                print('Line {}: Copy {} to {}'.format(l + 1, src, dest))
-                program.append((src, dest))
-
-        print('Program listing:')
-        for instr in program: print(instr)
+            print('{} {} | {} : {}'.format(addr, compiled, str(l + 1).rjust(max(lineNumSize, 4)), line))
